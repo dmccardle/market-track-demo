fa1a86f629de5b9cdc096c1af5e87e9d226ac368
dmccardle-mb <danny@mccardlebros.ca>
2025-03-24 19:56:45 -0300

    Added hardcoded heading to dashboard page

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, SimpleGrid } from "@chakra-ui/react"
+import { Box, Container, Flex, Heading, SimpleGrid } from "@chakra-ui/react"
 import { createFileRoute } from "@tanstack/react-router"
 
 import GraphPanel from "@/components/Dashboard/GraphPanel"
@@ -6,11 +6,14 @@ import InsightPanel from "@/components/Insights/InsightPanel"
 
 const Dashboard: React.FC = () => {
   return (
-    <Flex gap={2} direction={{ base: "column", md: "row" }}>
-      <Flex bg="green" flex={1}>
-        GraphPanel
+    <Flex direction="column" gap={1}>
+      <Heading size="3xl">March 19<sup>th</sup> - 26<sup>th</sup>, 2025</Heading>
+      <Flex gap={2} direction={{ base: "column", md: "row" }}>
+        <Flex bg="green" flex={1}>
+          GraphPanel
+        </Flex>
+        <InsightPanel />
       </Flex>
-      <InsightPanel />
     </Flex>
   );
 };
