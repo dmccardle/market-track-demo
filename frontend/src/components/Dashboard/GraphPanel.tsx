5bc23ba65e702d1c36fd67534269b20bd0ddafae
dmccardle-mb <danny@mccardlebros.ca>
2025-03-25 20:59:34 -0300

    Added labels & colouring to two graph layout

--- frontend/src/components/Dashboard/GraphPanel.tsx
+++ frontend/src/components/Dashboard/GraphPanel.tsx
@@ -1,24 +1,81 @@
 import { Container } from "@chakra-ui/react";
+import { ApexOptions } from "apexcharts";
 import ReactApexChart from "react-apexcharts";
 
 const GraphPanel = () => {
-  const options = {
-    chart: {
-      id: "basic-bar"
-    },
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-    }
-  };
-  const series = [
+  const cwtData: number[] = [1500, 1450, 1600, 1650, 1675];
+  const cwtColor: string = "#123456";
+  const priceData: number[] = [22, 22, 22.50, 22.50, 23];
+  const priceColor: string = "#654321";
+
+  const series: ApexAxisChartSeries = [
     {
-      name: "series-1",
-      data: [30, 40, 45, 50, 49, 60, 70, 91]
+      name: "cwt",
+      data: cwtData,
+      color: cwtColor
+    },
+    {
+      name: "avg.price",
+      data: priceData,
+      color: priceColor
     }
   ];
 
+  const options: ApexOptions = {
+    chart: {
+      id: "line"
+    },
+    stroke: {
+      curve: "straight"
+    },
+    markers: {
+      size: 8
+    },
+    xaxis: {
+      categories: ["February 26th", "March 5th", "March 12th", "March 19th", "March 26th"]
+    },
+    yaxis: [
+      {
+        labels: {
+          style: {
+            colors: cwtColor
+          }
+        },
+        title: {
+          text: "CWT",
+          rotate: 360,
+          style: {
+            color: cwtColor,
+          },
+          offsetX: -10,
+        },
+        // TODO: make range configurable
+        max: (Math.ceil(Math.max(...cwtData) + 50)),
+        min: (Math.floor(Math.min(...cwtData) - 50))
+      },
+      {
+        opposite: true,
+        labels: {
+          style: {
+            colors: priceColor
+          }
+        },
+        title: {
+          text: "Avg. Price",
+          rotate: 360,
+          style: {
+            color: priceColor
+          },
+          offsetX: 20,
+        },
+        max: (Math.ceil(Math.max(...priceData) + 0.25)),
+        min: (Math.floor(Math.min(...priceData) - 0.25))
+      }
+    ],
+  };
+
   return (
-    <Container>
+    <Container width={{ base: "100%", xl: "5xl" }}>
       <ReactApexChart
         type="line"
         options={options}
