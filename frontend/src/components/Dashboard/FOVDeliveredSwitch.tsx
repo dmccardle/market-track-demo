ba4be9754e3f6af76f6d02b2baa4584d184bf21f
dmccardle-mb <danny@mccardlebros.ca>
2025-03-25 08:38:35 -0300

    idk centering didn't work, will fix later lmao

--- frontend/src/components/Dashboard/FOVDeliveredSwitch.tsx
+++ frontend/src/components/Dashboard/FOVDeliveredSwitch.tsx
@@ -1,21 +1,21 @@
-import { HStack, Switch, Text } from "@chakra-ui/react";
+import { Flex, HStack, Switch, Text } from "@chakra-ui/react";
 import React, { useState } from "react"
 
 const FOVDeliveredSwitch: React.FC = () => {
   const [checked, setChecked] = useState(false);
   
   return (
-    <HStack gap={4}>
+    <Flex>
       {/* TODO: make this centered on the Switch, not the Switch & Text */}
-      <Text fontWeight={checked ? 'normal' : 'bold'} textStyle="xl" position="absolute" translate="-3em">FOB</Text>
+      <Text fontWeight={checked ? 'normal' : 'bold'} textStyle="xl" position="relative" translate="-0.5em">FOB</Text>
       <Switch.Root size="lg" variant="solid" checked={checked} onCheckedChange={(e) => setChecked(e.checked)}>
         <Switch.HiddenInput />
         <Switch.Control>
           <Switch.Thumb />
         </Switch.Control>
       </Switch.Root>
-      <Text fontWeight={checked ? 'bold' : 'normal'} textStyle="xl" position="absolute" translate="3em">Delivered</Text>
-    </HStack>
+      <Text fontWeight={checked ? 'bold' : 'normal'} textStyle="xl" position="relative" translate="0.5em">Delivered</Text>
+    </Flex>
   );
 };
 
