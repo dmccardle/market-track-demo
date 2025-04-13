a705bd96a12a45e54377d2a4bb696484802bd75d
dmccardle-mb <danny@mccardlebros.ca>
2025-04-13 14:19:45 -0300

    Added opening pdf in new tab on button press

--- frontend/src/routes/_layout/reports.tsx
+++ frontend/src/routes/_layout/reports.tsx
@@ -6,39 +6,38 @@ import React from "react";
 
 const Reports: React.FC = () => {
   const files: FileGridFile[] = [
-  {
-    heading: "Apr3-Apr9-2025",
-    text: "",
-  },
-  {
-    heading: "Mar27-Apr2-2025",
-    text: "",
-  },
-  {
-    heading: "Mar20-Mar26-2025",
-    text: "",
-  },
-  {
-    heading: "Mar13-Mar19-2025",
-    text: "",
-  },
-  {
-    heading: "Mar6-Mar12-2025",
-    text: "",
-  },
-  {
-    heading: "Feb27-Mar5-2025",
-    text: "",
-  },
-];
+    {
+      heading: "Apr3-Apr9-2025",
+      text: "",
+    },
+    {
+      heading: "Mar27-Apr2-2025",
+      text: "",
+    },
+    {
+      heading: "Mar20-Mar26-2025",
+      text: "",
+    },
+    {
+      heading: "Mar13-Mar19-2025",
+      text: "",
+    },
+    {
+      heading: "Mar6-Mar12-2025",
+      text: "",
+    },
+    {
+      heading: "Feb27-Mar5-2025",
+      text: "",
+    },
+  ];
+
+  const openPdfInNewTab = () => {
+    window.open(ReportPDF, "_blank");
+  }
 
   return (
-    <>
-      <FileGrid files={files} cardsIcon="fa-reg-file-pdf" buttonsIcon="md-outline-file-open" />
-      <a href={ReportPDF} target="_blank">
-        Open pdf
-      </a>
-    </>
+    <FileGrid files={files} cardsIcon="fa-reg-file-pdf" buttonsIcon="md-outline-file-open" buttonsPress={openPdfInNewTab} />
   );
 };
 
