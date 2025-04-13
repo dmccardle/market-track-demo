c062038b72f84dad032bc7fe6a1c2b6ac4b133c6
dmccardle-mb <danny@mccardlebros.ca>
2025-04-13 14:05:11 -0300

    Improved header styling

--- frontend/src/components/Common/Navbar.tsx
+++ frontend/src/components/Common/Navbar.tsx
@@ -8,11 +8,13 @@ function Navbar() {
       position="sticky"
       color="white"
       align="center"
-      bg="gray.300"
+      bg="gray.100"
       w="100%"
+      shadow="md"
       top={0}
       p={4}
       gap={4}
+      zIndex={1}
     >
       <Image height={{ base: "3em", md: "3.5em" }} src="/assets/images/PEIPotatoLogo.png" />
       <Heading size={{ base: "xl", sm: "2xl", md: "3xl", lg: "4xl" }} color="black">PEI Potato Market Track</Heading>
