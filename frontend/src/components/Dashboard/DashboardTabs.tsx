244e9669ebeee2fc761b05c9485be9e7de6d0993
dmccardle-mb <danny@mccardlebros.ca>
2025-04-28 20:48:07 -0300

    Added loading of all 3 dummy data points

--- frontend/src/components/Dashboard/DashboardTabs.tsx
+++ frontend/src/components/Dashboard/DashboardTabs.tsx
@@ -3,9 +3,15 @@ import React, { useMemo, useState } from "react"
 import { FaBox, FaStore, FaTruckMoving } from "react-icons/fa";
 import DashboardContent from "./DashboardContent";
 import { plainToClass } from "class-transformer";
-import consumerJson from "../../data/dummy-consumer-1.json";
-import countJson from "../../data/dummy-count-1.json";
-import bulkJson from "../../data/dummy-bulk-1.json";
+import consumerJson1 from "../../data/dummy-consumer-1.json";
+import consumerJson2 from "../../data/dummy-consumer-2.json";
+import consumerJson3 from "../../data/dummy-consumer-3.json";
+import countJson1 from "../../data/dummy-count-1.json";
+import countJson2 from "../../data/dummy-count-2.json";
+import countJson3 from "../../data/dummy-count-3.json";
+import bulkJson1 from "../../data/dummy-bulk-1.json";
+import bulkJson2 from "../../data/dummy-bulk-2.json";
+import bulkJson3 from "../../data/dummy-bulk-3.json";
 import MarketData from "@/data/MarketData";
 import { useVariety } from "@/contextProviders/VarietyProvider";
 import { useDesintation } from "@/contextProviders/DestinationProvider";
@@ -35,13 +41,19 @@ const DashboardTabs: React.FC = () => {
   const varietyContext = useVariety();
   const destinationContext = useDesintation();
 
-  const dummyConsumer: MarketData = plainToClass(MarketData, consumerJson);
-  const dummyCount: MarketData = plainToClass(MarketData, countJson)
-  const dummyBulk: MarketData = plainToClass(MarketData, bulkJson);
+  const dummyConsumer1: MarketData = plainToClass(MarketData, consumerJson1);
+  const dummyConsumer2: MarketData = plainToClass(MarketData, consumerJson2);
+  const dummyConsumer3: MarketData = plainToClass(MarketData, consumerJson3);
+  const dummyCount1: MarketData = plainToClass(MarketData, countJson1)
+  const dummyCount2: MarketData = plainToClass(MarketData, countJson2)
+  const dummyCount3: MarketData = plainToClass(MarketData, countJson3)
+  const dummyBulk1: MarketData = plainToClass(MarketData, bulkJson1);
+  const dummyBulk2: MarketData = plainToClass(MarketData, bulkJson2);
+  const dummyBulk3: MarketData = plainToClass(MarketData, bulkJson3);
 
-  const consumerData: MarketData[] = [dummyConsumer];
-  const countData: MarketData[] = [dummyCount];
-  const bulkData: MarketData[] = [dummyBulk];
+  const consumerData: MarketData[] = [dummyConsumer1, dummyConsumer2, dummyConsumer3];
+  const countData: MarketData[] = [dummyCount1, dummyCount2, dummyCount3];
+  const bulkData: MarketData[] = [dummyBulk1, dummyBulk2, dummyBulk3];
 
   const consumerLists: ListData = useMemo(() => getVarietyAndDestinationLists(consumerData), consumerData);
   const countLists: ListData = useMemo(() => getVarietyAndDestinationLists(countData), countData);
